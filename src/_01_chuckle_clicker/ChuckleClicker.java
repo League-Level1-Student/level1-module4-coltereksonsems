e03a8d20d21a311e8371774d1e6d97d37e05c0ea
root <root@3c73acbacb1d>
2023-01-10 02:27:00 +0000

    Start of ChuckleClicker.

---
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -0,0 +1,28 @@
+package _01_chuckle_clicker;
+
+
+
+public class ChuckleClicker {
+public static void main(String[] args) {
+	
+	Jframe
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+void makeButtons(){
+	
+}
+}
