93819c8c6ba6f07b7001e0b7d820a9758fa0ca0b
root <root@9003476b77bd>
2023-06-27 01:28:39 +0000

    more of typingclass

--- src/_03_typing_tutor/typingclass.java
+++ src/_03_typing_tutor/typingclass.java
@@ -1,5 +1,33 @@
 package _03_typing_tutor;
+
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 setup()
 public class typingclass {
+	char generateRandomLetter() {
+	    Random r = new Random();
+	    return (char) (r.nextInt(26) + 'a');
+	}
+
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+char currentLetter;
+
+void setup(){
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+
+
+
+
+
+
+
+
+
 
 }
